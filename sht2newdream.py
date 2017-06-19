46c3054a18adcba4672821e5de56921cf453e253
Liu Jun <liujun.richard@gmail.com>
2017-06-19 01:03:53 -0800

    added wifi scan

--- sht2newdream.py
+++ sht2newdream.py
@@ -8,31 +8,83 @@
 import smbus
 import time
 import paho.mqtt.client as mqtt
+import uuid
+import os
+import sys
+from wifi import Cell, Scheme
+from wifi.utils import print_table, match as fuzzy_match
 
 bus = smbus.SMBus(1)
 SHT_ADDR = 0x44
+global connected
+connected = 0
 
 def on_disconnect(client, obj, rc):
-    client.reconnect()
+    print "Broker connection failed..."
+    # global connected
+    # connected = 0
+    while 1:
+        try:
+            client.reconnect()
+        except Exception, e:
+            continue
+        break
+
+def on_connect(client, userdata, rc):
+    print "Broker connected!"
+    # global connected
+    # connected = 1
 
 def sht_read():
     bus.write_i2c_block_data(SHT_ADDR, 0x24, [0x00])
-    time.sleep(0.1)
+    time.sleep(0.05)
     data = bus.read_i2c_block_data(SHT_ADDR, 0,6)
     temp = ((( data[0] * 256.0 + data[1]) * 175 ) / 65535.0) - 45
     humid = ((( data[3] * 256.0 + data[4]) * 100 ) / 65535.0)
     print "T = %6.2f   RH = %6.2f %%" % (temp,humid)
     return temp,humid
 
-if __name__ == "__main__":
-    client = mqtt.Client()
-    client.on_disconnect = on_disconnect
-    client.connect("www.newdream.xyz", 1883)
+def comp3(x,y):
+    if x[0]<y[0]:
+        return 1
+    else:
+        return -1
 
+if __name__ == "__main__":
+    mac=uuid.UUID(int = uuid.getnode()).hex[-12:]
+    print "MAC:"+ mac
+
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_disconnect = on_disconnect
+    print "Try connecting to server..."
+    client.connect("www.newdream.xyz", 1883, 10)
+    client.loop_start()
+
+    if len(sys.argv) > 1:
+        interface = sys.argv[1]
+    else:
+        interface = 'wlan0'
+   
+    wifi_scan_result = [[cell.signal, cell.ssid, cell.address] for cell in Cell.all(interface)] 
+    countdown_1min=2
     while 1:
         t,h = sht_read()
-        client.publish("td/room615/temp/1", "%.2f" % t)
-	client.publish("td/room615/humid/1", "%.2f" % h)
-        time.sleep(60)
+        try:
+            # client.publish("td/room615/temp/1", "%.2f" % t)
+            # client.publish("td/room615/humid/1", "%.2f" % h)
+            client.publish("td/room615/sensor/1",'{"dev":"RPi","id":"001","mac":"%s","data":{"temp":%.2f,"humid":%.2f}}' % (mac,t,h)) 
+            if countdown_1min == 0:
+                countdown_1min=60
+                wifi_scan_result = [[cell.signal, cell.ssid, cell.address] for cell in Cell.all(interface)]
+                wifi_scan_result.sort(comp3)
+                wifi_scan_result = wifi_scan_result[:20]
+                print wifi_scan_result
+                client.publish("td/room615/sensor/1",'{"dev":"RPi","id":"001","mac":"%s","wifi_scan":"%s"}' % (mac,wifi_scan_result))
+        except Exception, e:
+            print e
+        time.sleep(0.95)
+        if countdown_1min>0:
+            countdown_1min = countdown_1min - 1
 
 
